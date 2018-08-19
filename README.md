e6661dcc2ad49d830d6980c7439b79fe221429ea
Edson Celio <edsoncelio@protonmail.com>
2018-08-18 22:41:21 -0300

    Versão de Testes

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ Bot do Telegram para o *StackOverFlow* em português (SOpt).
 #### Funcionalidades básicas
 * Consulta  e notificação de questões por `tags` e palavras-chaves, exibição no formato:
 	* Título, `tag`, e *link* para acesso.
+	
 
 #### Implementações futuras
-* Autenticação via *OAuth*, usando a API oficial, para notificação de comentários e votos do usuário logado.
+
+* Autenticação via *OAuth*, usando a API oficial, para notificação de novos comentários e votos do usuário logado.
+* Postagem das questões mais recentes em um canal
+* Opções de uso da conta: votar em questões, sinalizar 
 
 #### Instalação e configuração
---
+++ src/bot.py
@@ -0,0 +1,68 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+from sopt import buscar_questoes
+import json
+import os
+
+# Habilitar o logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(bot, update):
+    """Mensagem inicial do comando /start"""
+    update.message.reply_text('Iniciar configuração - mostrar lista de comandos')
+
+def ver_perguntas(bot, update):
+	#reply_keyboard = [['python', 'php', 'javascript']]
+	
+	#buscar questao da lista de mais recentes da tag 'python'(intervalo de 1 dia)
+	questoes = buscar_questoes()
+	update.message.reply_text(questoes[0])
+	
+def help(bot, update):
+    update.message.reply_text("""
+    Bot para perguntas do SOpt - em construção!
+    """)
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text("Comando inválido - ver lista de comandos")
+
+
+def error(bot, update, error):
+    """Log de Erros"""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Iniciar o bot"""
+    # Criar o EventHandler
+    config = json.loads(open(os.getcwd()[0:int(len(os.getcwd())-4)]+"/config/token.json").read())
+    updater = Updater(config["token"])
+
+    # Obter o dispatcher para registrar os handlers
+    dp = updater.dispatcher
+
+    # comandos habilitados
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("buscar", ver_perguntas))
+
+    # p/ comandos não reconhecidos
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log de erros
+    dp.add_error_handler(error)
+
+    # Iniciar o Bot
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
