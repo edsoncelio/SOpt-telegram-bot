7c3c6a563fd7b3a579fcb2f1db67d88448e6f091
Edson Celio <edsoncelio@users.noreply.github.com>
2018-10-04 07:40:35 -0300

    Update test_sopt.py

--- tests/test_sopt.py
+++ tests/test_sopt.py
@@ -1,9 +1,9 @@
 from stackapi import StackAPI
 
 
-def test_buscar_questoes():
+def test_buscar_questoes(tags):
     sopt = StackAPI("pt.stackoverflow")
     sopt.page_size = 100
     sopt.max_pages = 1
-    questoes_python = sopt.fetch('questions', min=1, fromdate=1534582800, todate=1534636800, tagged='python')
+    questoes_python = sopt.fetch('questions', min=1, fromdate=1534582800, todate=1534636800)
     assert 1534625951 == questoes_python['items'][0]['creation_date']
